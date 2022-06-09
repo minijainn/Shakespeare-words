11e55f2ab8ebc791335a9490ed275735719b0a5d
minijainn <minijainn293@gmail.com>
2022-06-09 23:10:53 +0530

    update:take input and console

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log("This is a console window");
-var userName=prompt("Whuts your name ?");
-alert("Welcome "+userName+" to minion talks");
+// console.log("This is a console window");
+// var userName=prompt("Whuts your name ?");
+// alert("Welcome "+userName+" to minion talks");
+
+
+// btnTranslate.addEventListener("click",function translator(){
+//     console.log("hi you clicked");
+// })
+function translator(){
+console.log("hi");
+console.log(txtInput.value);
+}
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+btnTranslate.addEventListener("click",translator);
